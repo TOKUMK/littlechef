66bc17ba5ac2af24d9dd7ecac92563613e94b550
TOKUMK <jcaseyire@gmail.com>
2017-05-04 15:51:55 +0700

    initial commit

---
+++ recipes/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+# Create your models here.
+
+class Recipe(models.Model):
+	pass
+
+
+
+class RecipeSteps(models.Model):
+	pass
+
+
+
+
+class RecipeStepsIngredients(models.Model):
+	pass
+
+
+
+class Ingredients(models.Model):
+	pass
+
+
+class IngredientsType(models.Model):
+	pass
+
+
+
+
