deb57d8df2f7254f1767f7569175c91320df0ce2
TOKUMK <jcaseyire@gmail.com>
2017-05-04 19:09:27 +0700

    update models and admin models

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Recipe, RecipeStep, RecipeStepIngredient, Ingredient, IngredientType
 
-# Register your models here.
+admin.site.register(Recipe)
+admin.site.register(RecipeStep)
+admin.site.register(RecipeStepIngredient)
+admin.site.register(Ingredient)
+admin.site.register(IngredientType)
--- recipes/models.py
+++ recipes/models.py
@@ -4,41 +4,67 @@ from django.db import models
 
 class Recipe(models.Model):
 	
-	#recipe_id = (pk)
+	#recipe_id = pk
 
 	recipe_name = models.CharField(max_length=200, help_text="Enter a recipe title")
 	recipe_description = models.CharField(max_length=1000, help_text="enter a breif description of the recipe")
+
+	def __str__(self):
+		return '%s' % (self.recipe_name)
 	
 
-class RecipeSteps(models.Model):
+class RecipeStep(models.Model):
 	
 	#recipe_id = (fk)
+	recipe = models.ForeignKey('Recipe', on_delete=models.SET_NULL, null=True)
 	step_number = models.IntegerField()
 	instructions = models.CharField(max_length=400, help_text="Enter a instruction for recipe step")
 
+	def __str__(self):
+
+		return '%s Step: %s' % (self.recipe.recipe_name, self.step_number)
 
 
-class RecipeStepsIngredients(models.Model):
+
+
+
+class RecipeStepIngredient(models.Model):
 
 	#recipe_id = (fk)
 	#step_number = (fk)
 	#ingreditent_id = (fk) 
+
+	recipe = models.ForeignKey('Recipe', on_delete=models.SET_NULL, null=True)
+	recipe_step = models.ForeignKey('RecipeStep', on_delete=models.SET_NULL, null=True)
+	ingredient = models.ForeignKey('Ingredient', on_delete=models.SET_NULL, null=True)
+
 	amount_required = models.IntegerField()
 
+	def __str__(self):
+		return '%s' % (self.ingredient.ingredient_name)
 
 
-class Ingredients(models.Model):
+
+class Ingredient(models.Model):
 	
 	#ingredient_id = (pk)
-	#ingredient_type_code = (fk)
+
+	ingredient_type = models.ForeignKey('IngredientType', on_delete=models.SET_NULL, null=True)
+
 	ingredient_name = models.CharField(max_length=50, help_text="Enter an ingredient name")
+	def __str__(self):
+		return '%s' % (self.ingredient_name)
 	
 	
 
 
-class IngredientsType(models.Model):
+class IngredientType(models.Model):
 	#ingredient_type_code = (pk)
+
 	ingredient_type_description = models.CharField(max_length = 500, help_text="Enter a description of ingredient type")
+
+	def __str__(self):
+		return '%s' % (self.ingredient_type_description)
 
 
 
