da6889f35417cfaff4a884cc8b76694ef64554d6
TOKUMK <jcaseyire@gmail.com>
2017-05-04 16:19:40 +0700

    update system models to reflect database design

--- recipes/models.py
+++ recipes/models.py
@@ -3,27 +3,43 @@ from django.db import models
 # Create your models here.
 
 class Recipe(models.Model):
-	pass
+	
+	#recipe_id = (pk)
 
-
+	recipe_name = models.CharField(max_length=200, help_text="Enter a recipe title")
+	recipe_description = models.CharField(max_length=1000, help_text="enter a breif description of the recipe")
+	
 
 class RecipeSteps(models.Model):
-	pass
-
+	
+	#recipe_id = (fk)
+	step_number = models.IntegerField()
+	instructions = models.CharField(max_length=400, help_text="Enter a instruction for recipe step")
 
 
 
 class RecipeStepsIngredients(models.Model):
-	pass
+
+	#recipe_id = (fk)
+	#step_number = (fk)
+	#ingreditent_id = (fk) 
+	amount_required = models.IntegerField()
 
 
 
 class Ingredients(models.Model):
-	pass
+	
+	#ingredient_id = (pk)
+	#ingredient_type_code = (fk)
+	ingredient_name = models.CharField(max_length=50, help_text="Enter an ingredient name")
+	
+	
 
 
 class IngredientsType(models.Model):
-	pass
+	#ingredient_type_code = (pk)
+	ingredient_type_description = models.CharField(max_length = 500, help_text="Enter a description of ingredient type")
+
 
 
 
